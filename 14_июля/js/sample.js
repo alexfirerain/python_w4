ff0efd5e01f2ca839af998cd07efd54bc75ce998
alexfirerain <alexfirerain@yandex.ru>
2025-07-14 11:19:49 +0300

    javascript intro

---
+++ 14_июля/js/sample.js
@@ -0,0 +1,49 @@
+// скрипт пример на джаваскрипт
+
+/*
+   ***********************************
+    здесь некоторые элементы языка
+   ***********************************
+
+   learn.javascript.ru
+   язык съ слабой типизацией, и с почти динамической
+   свой интерпретатор в каждом браузере
+
+   var - традиционное, но уже устаревшее ?!
+        въ всяком разе такая переменная видна глобально
+   let - новый способ объявления, она видна только в своей области
+
+   функция определяется до использования
+   document - доступ к документу
+*/
+
+function changeColor() {
+    document.getElementById("alive").style.color = "red";
+    const txt = document.getElementById("alive");
+    if (txt.style.display === 'none') {
+        txt.style.display = block;
+    } else {
+    txt.style.display = 'none'}
+}
+
+// подключаюсь к элементу ДОМа
+const txt = document.getElementById("alive").onClick = changeColor;
+
+
+
+
+function sayHello(name) {
+    document.writeln("Вас зовут " + name);  // .write() то же без перевода строки
+}
+
+
+let name = prompt("Ваше имя: ");
+sayHello(name);
+
+let colors = ["Красный", "Синий", "Голубой"];
+
+document.writeln("<h1>Цвета</h1><ul>");
+for(let i=0; i<colors.length; i++) {
+    document.writeln("<li>" + colors[i] + "</li>");
+}
+document.writeln("</ul>");
